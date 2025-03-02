c2561d79291b76ecadc962ffd18b0fec9ed56647
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-02 22:06:03 +0530

    add layouts

--- layout_app_2/lib/main.dart
+++ layout_app_2/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,7 +10,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Layout',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                color: Colors.green,
+                height: 350,
+                width: 360,
+                child: Center(
+                  child: Text(
+                    'Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons. Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(color: Colors.green, height: 350, width: 360),
+          ],
+        ),
+      ),
     );
   }
 }
