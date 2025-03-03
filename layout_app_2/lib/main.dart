def70e618a39478ac993a48f148b580d828cf356
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-03 12:06:18 +0530

    modify texts

--- layout_app_2/lib/main.dart
+++ layout_app_2/lib/main.dart
@@ -13,10 +13,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          leading: Icon(Icons.menu, color: Colors.white, size: 30, ),
           title: Text(
             'Layout',
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
           ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+              child: Icon(Icons.search, color: Colors.white, size: 30,),
+            ),
+          ],
           backgroundColor: Colors.blue,
         ),
 
@@ -26,18 +33,39 @@ class MyApp extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(15.0),
               child: Container(
-                color: Colors.green,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20,),
+                  color: Colors.green,
+                ),
                 height: 350,
                 width: 360,
-                child: Center(
-                  child: Text(
-                    'Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons. Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons.',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 15, 15, 5),
+                      child: Text(
+                        'Title',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 5, 15, 15),
+                      child: Text(
+                        'Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons. Flutter provides css classes for large number of icons which can be easily integrated with any web application. We have prepared list of icons which you can simply copy paste into your website. Customization of these icons is also given so that you can easily modify look and feel of Flutter Icons.',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ),
             ),
             Container(color: Colors.green, height: 350, width: 360),
